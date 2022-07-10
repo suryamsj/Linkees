bab1bb51b472e1ce4f8db960e6e272a341af314c
Muhammad Surya Jayadiprana <msurya672@gmail.com>
2022-07-10 07:50:43 +0700

    Linkees :airplane:
    Update link

--- src/App.svelte
+++ src/App.svelte
@@ -64,7 +64,7 @@
       <div class="col-lg-4">
         <Cards
           title="Telegram"
-          url="https://telegram.me/suryaamsj"
+          url="https://telegram.me/suryaaamsj"
           description="@suryaaamsj | Chat dengan saya"
           image={telegram}
         />
